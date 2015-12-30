0ecc2833181bd2a82f4a91d12f2a956beef9de7a
MitchJH <mheastie@hotmail.co.uk>
2015-12-30 21:01:11 +0000

    Removed debug command lines from program.cs

--- BaseBuilder/BaseBuilder/BaseBuilder/Program.cs
+++ BaseBuilder/BaseBuilder/BaseBuilder/Program.cs
@@ -10,13 +10,6 @@ namespace BaseBuilder
         /// </summary>
         static void Main(string[] args)
         {
-            args = new string[5];
-            args[0] = "window_mode Borderless";
-            args[1] = "x_res 1680";
-            args[2] = "y_res 1050";
-            args[3] = "window_width 1680";
-            args[4] = "window_height 1050";
-
             using (Engine game = new Engine(args))
             {
                 game.Run();
